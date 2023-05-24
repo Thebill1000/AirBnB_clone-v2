59907161296d33efbc7ac1b7e8f918e55f286684
thebill1000 <williamsbilljnr100@gmail.com>
2023-05-24 03:58:52 -0700

    deployed

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-# -*- coding: utf-8 -*-
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+"""
 from flask import Flask
+
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """Start a basic Flask web application"""
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Adding a specific route /hbnb"""
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0" port="5000") 
